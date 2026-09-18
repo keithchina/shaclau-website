8e7ed867ec69949c27699f7056c04311f00b76f6
China <shaclaultd@gmail.com>
2026-09-19 00:08:14 +0300

    testing renaming

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -135,10 +135,10 @@ const projects: Project[] = [
     outcome:
       'Verified, accurate level data delivered throughout construction, giving the site team confidence in cut depths, grading, and compaction as work progressed.',
     images: [
-      '/images/projects/leveling/01.jpeg',
-      '/images/projects/leveling/02.jpg',
-      '/images/projects/leveling/03.jpg',
-      '/images/projects/leveling/07.jpeg',
+      '/images/projects/level-survey/01.jpg',
+      '/images/projects/level-survey/02.jpg',
+      '/images/projects/level-survey/03.jpg',
+      '/images/projects/setting-out/07.jpg',
     ],
   },
 ];
